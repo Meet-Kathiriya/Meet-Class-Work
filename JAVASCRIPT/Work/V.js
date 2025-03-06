d79bd89ef385e78d918840fd3ff7e8ab30ca5ebc
Meet-Kathiriya <zyxcab101819@gmail.com>
2025-03-06 09:34:59 +0530

    abcd

---
+++ JAVASCRIPT/Work/V.js
@@ -0,0 +1,62 @@
+// // Vowel With Ladder (If Else)
+
+// let char = prompt("Enter a single character:").toLowerCase();
+
+// if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+//     alert(char + " is Vowel");
+//     console.log(char + " is Vowel");
+// }
+// else {
+//     alert(char + " is not Vowel");
+//     console.log(char + " is not Vowel");
+// }
+
+
+let a = prompt("Enter a charachter")
+
+switch (a) {
+    case "A":
+        alert("Vowels");
+        break;
+
+    case "E":
+        alert("Vowels");
+        break;
+
+    case "I":
+        alert("Vowels");
+        break;
+
+    case "O":
+        alert("Vowels");     
+        break;
+
+    case "U":
+        alert("Vowels");        
+        break;
+
+    case "a":
+        alert("Vowels");
+        break;
+
+    case "e":
+        alert("Vowels");
+        break;
+
+    case "i":
+        alert("Vowels");
+        break;
+
+    case "o":
+        alert("Vowels");     
+        break;
+
+    case "u":
+        alert("Vowels");        
+        break;
+
+default :
+    alert("Not Vowels")
+
+    
+}
