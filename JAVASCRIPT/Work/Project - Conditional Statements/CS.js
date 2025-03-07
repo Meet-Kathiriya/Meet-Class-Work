ee281c203c1226dfe069bfe3481302c057e3c041
Meet-Kathiriya <zyxcab101819@gmail.com>
2025-03-07 11:48:53 +0530

    Conditional Statements

---
+++ JAVASCRIPT/Work/Project - Conditional Statements/CS.js
@@ -0,0 +1,216 @@
+// Data Type - Primitive -->>
+
+// let a = 101;
+
+// let b = "MeeT "
+
+// let c = False
+
+// let d = Symbol("abc" , 123)
+
+// let e = BigInt(64554642)
+
+// console.log("MeeT");
+
+// console.log(a);
+
+// console.log(b);
+
+// console.log(c);
+
+// console.log(d);
+
+// console.log(typeof(a));
+
+// console.log(typeof(b));
+
+// console.log(typeof(c));
+
+// console.log(typeof(d));
+
+
+
+//----------------------------------------------------------------------------------------------------------------
+
+
+
+// Operators -->>
+
+// (1) Assignment Operator :-
+    // = , += , -= , *= , /= , %= , =
+
+    // let a = 7;
+
+    // console.log(a);
+
+    // a += 5;
+    // console.log(a);
+
+    // a -= 5;
+    // console.log(a);
+    
+    // a *= 2;
+    // console.log(a);
+
+    // a /= 2;
+    // console.log(a);
+
+    // a %= 2;
+    // console.log(a);
+
+    // a = 2;
+    // console.log(a);
+
+
+
+//----------------------------------------------------------------------------------------------------------------
+
+
+
+// (2) Arithmatic Operator -->>
+    // + , - , * , / , % , ++ , -- , 
+
+    // let x = 2;
+    // let y = 3;
+
+    // console.log(x + y);
+    // console.log(x - y);
+    // console.log(x * y);
+    // console.log(x / y);  
+    // console.log(x % y);
+    // console.log(++x);
+    // console.log(--y);
+    // console.log(x  y);
+
+
+
+//----------------------------------------------------------------------------------------------------------------
+
+
+
+// (3) Comparison Operator -->>
+    // > , < , >= , <= , == , === , != , !==
+
+    // let a = 1;
+    // let b = 5;
+
+    // console.log(a == b);
+    // console.log(a != b);
+    // console.log(a === b);
+    // console.log(a !== b);
+    // console.log(a > b);
+    // console.log(a < b);
+    // console.log(a >= b);
+    // console.log(a <= b);
+
+
+
+//----------------------------------------------------------------------------------------------------------------
+
+
+
+// (4) Logical Operator
+    // && ,  , !
+
+    // let a = 5;
+    // let b = 10;
+
+    // console.log(a < b && a === 5);
+    // console.log(a < b  a === 10);
+    // console.log(!(a < b));
+    
+
+
+//----------------------------------------------------------------------------------------------------------------
+
+
+
+// let number = parseInt(prompt("Enter a Number:-"));
+
+
+// if (number % 2 === 0){
+// alert (number + " is even");
+// console.log(number + " is even");
+
+// } else {
+//     alert(number + " is odd");
+//     console.log(number + " is odd");
+// }
+
+
+
+// let a = prompt("Enter 1 for order Pizza and 2 for Burger");
+
+// if (a == 1) {
+//     console.log("You Ordered a Pizza 🍕");
+    
+// }else {
+//     console.log("You Ordered a Burger 🍔");
+    
+// }
+
+// if (a == 1) {
+//     console.log("You Ordered a Pizza 🍕");
+// }else if (a == 2) {
+//     console.log("You Ordered a Burger 🍔");
+// }else {
+//     console.log("Your Choice is Wrong ❌");
+    
+// }
+
+
+
+// let age = 17;
+
+// if (age >= 18) {
+//     if (age <= 17) {
+//         console.log("You can't Drive a Vehicle.");
+//     } else {
+//         console.log("You can Drive a Vehicle.");
+//     }
+// } else {
+//     console.log("You are too. young to Drive a Vehicle");
+// }
+
+
+
+// let a = 5;
+// let b = 2;
+// let c = 10;
+
+// if (a <= b) {
+//     if (a <= c) {
+//         console.log("a is Min");
+        
+//     }else {
+//         console.log("c is Min");
+        
+//     }
+// }else {
+//     if (b <= c) {
+//         console.log("b is Min");
+//     }else {
+//         console.log("c is Min");
+//     }
+// }
+
+
+
+// let a = 10;
+// let b = 50;
+// let c;
+
+// c = a;
+// a = b;
+// b = c;
+
+// console.log ("a =", a, "b =", b);
+
+
+
+// let a = 15;
+// let b = 10;
+
+// [a,b] = [b,a];
+
+// console.log(a,b);
